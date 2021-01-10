e144a96278fffecfd8340452e7a685b271ba83d2
manoj0910 <manoj.saini@metacube.com>
2021-01-10 15:31:52 +0530

    test commit

--- src/main/java/com/manu/springmail/HomeController.java
+++ src/main/java/com/manu/springmail/HomeController.java
@@ -23,7 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfWriter;
